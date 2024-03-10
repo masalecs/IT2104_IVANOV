97d4670687ba236919390d5ee13263d53ac33695
MasAlecs <alecs2018,ru@mail.ru>
2024-03-11 00:06:57 +0300

    Добавлена верхняя часть описания и функция Лайк

--- laba4/lib/main.dart
+++ laba4/lib/main.dart
@@ -24,26 +24,65 @@ class DormitoryPage extends StatefulWidget {
 class _DormitoryPageState extends State<DormitoryPage> {
   int likes = 0;
 
+  void incrementLikes() {
+    setState(() {
+      likes++;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Общежития КубГАУ"),
-        backgroundColor: Colors.green,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.network(
-              'https://sun6-20.userapi.com/s/v1/if2/5KzWOm7-zuFbvIsFRwKpM6sl2YLZ1VhRFVL0OK1BQjEHZIqp0PYjKQ2hk9eJ36POQUwlGwXQ47KOTT0OyW-xiQ9y.jpg?size=200x200&quality=96&crop=286,118,761,761&ava=1',
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Общежития КубГАУ"),
+          backgroundColor: Colors.green,
         ),
-      ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.network(
+                'https://sun6-20.userapi.com/s/v1/if2/5KzWOm7-zuFbvIsFRwKpM6sl2YLZ1VhRFVL0OK1BQjEHZIqp0PYjKQ2hk9eJ36POQUwlGwXQ47KOTT0OyW-xiQ9y.jpg?size=200x200&quality=96&crop=286,118,761,761&ava=1',
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Общежитие 20",
+                      style: TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "г. Краснодар, ул. Калинина 13/20",
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.favorite, color: Colors.red),
+                              onPressed: incrementLikes,
+                            ),
+                            Text("$likes"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
