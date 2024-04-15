8f15517d89f30c4f2658991311f59afc93e2f535
MasAlecs <alecs2018,ru@mail.ru>
2024-04-15 22:00:57 +0300

    Добавлено первое текствое поле

---
+++ Mobile-Devs-lab6branch/lab6/lib/MyCustomForm.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lab6/main.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Рассчет площади';
+
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
---
+++ Mobile-Devs-lab6branch/lab6/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:lab6/MyCustomForm.dart';
+
+void main() => runApp(MyApp());
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>(); 
+
+  String _str = "";
+
+  void _calculate() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _str =
+            'Area is equal to ${(int.parse(f1.text) * int.parse(f2.text)).toString()}';
+      });
+
+      SnackBar snackBar = const SnackBar(content: Text('Processing complete!'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      setState(() {
+        _str = "";
+      });
+    }
+  }
+
+  final f1 = TextEditingController();
+  final f2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(6.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  const Expanded(child: Text("Высота(mm^2):"), flex: 10),
+                  Expanded(
+                      flex: 29,
+                      child: TextFormField(
+                        controller: f1,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Введите высоту';
+                          }
+                          return null;
+                        },
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
