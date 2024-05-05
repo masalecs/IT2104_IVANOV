f993e3ed23d10c6a0e8c535ad6bbfb77dadcbeb8
MasAlecs <alecs2018,ru@mail.ru>
2024-05-05 20:59:52 +0300

    Создал файл main

---
+++ lab9/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Machine App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final TextEditingController _controller = TextEditingController();
+
+  }
+
+
+
+
+}
